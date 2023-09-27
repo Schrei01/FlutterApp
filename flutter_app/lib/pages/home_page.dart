6c7657f23d3b2039736bca394f91b1fdeca64e5f
Alejandro Vargas <fravarga@bancolombia.com.co>
2023-09-26 21:40:10 -0500

    Se termina el maquetado de la pantalla 1

--- flutter_app/lib/pages/home_page.dart
+++ flutter_app/lib/pages/home_page.dart
@@ -4,9 +4,36 @@ import 'package:flutter_app/design/radius.dart';
 import 'package:flutter_app/widgets/custom_money_display.dart';
 import 'package:flutter_app/widgets/home_app_title.dart';
 import 'package:flutter_app/widgets/summary_card.dart';
+import 'package:flutter_app/widgets/product_detail.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var buttonStyleInactive = ElevatedButton.styleFrom(
+      elevation: 0,
+      backgroundColor: AppColors.brandLightColor,
+      shadowColor: AppColors.brandLightColor);
+  var buttonStyleActive = ElevatedButton.styleFrom(
+      backgroundColor: AppColors.brandSecondaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12), // <-- Radius
+      ));
+  Widget currentDetailWidget = const CategoriesWidget();
+  late ButtonStyle categorieBtnStyle;
+  late ButtonStyle recentTransactionsBtnStyle;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    categorieBtnStyle = buttonStyleActive;
+    recentTransactionsBtnStyle = buttonStyleInactive;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +48,28 @@ class HomePage extends StatelessWidget {
             padding: EdgeInsets.fromLTRB(16, 45, 16, 12),
             child: HomeAppBarTitle()),
       ),
-      body: const Column(
+      body: Column(
         children: [
-          TopHomePage(),
+          const TopHomePage(),
+          MidHomePageBody(
+            categorieBtnStyle: categorieBtnStyle,
+            recentTransactionsBtnStyle: recentTransactionsBtnStyle,
+            categoriesBtnAction: () {
+              setState(() {
+                currentDetailWidget = const CategoriesWidget();
+                categorieBtnStyle = buttonStyleActive;
+                recentTransactionsBtnStyle = buttonStyleInactive;
+              });
+            },
+            recentBtnAction: () {
+              setState(() {
+                categorieBtnStyle = buttonStyleInactive;
+                recentTransactionsBtnStyle = buttonStyleActive;
+                currentDetailWidget = const RecentTransactions();
+              });
+            },
+          ),
+          Expanded(child: currentDetailWidget)
         ],
       ),
     );
@@ -73,6 +119,122 @@ class TopHomePage extends StatelessWidget {
           action: () => print('spending'),
         ),
       ]),
+    );
+  }
+}
+
+class MidHomePageBody extends StatelessWidget {
+  final ButtonStyle categorieBtnStyle;
+  final ButtonStyle recentTransactionsBtnStyle;
+  final void Function()? categoriesBtnAction;
+  final void Function()? recentBtnAction;
+  const MidHomePageBody(
+      {super.key,
+      required this.categorieBtnStyle,
+      required this.recentTransactionsBtnStyle,
+      this.categoriesBtnAction,
+      this.recentBtnAction});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.only(left: 16, right: 16),
+        height: 104,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: AppColors.brandLightColor,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                bottomRight: Radius.circular(16))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ElevatedButton(
+                style: categorieBtnStyle,
+                onPressed: categoriesBtnAction,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 40),
+                  child: Text(
+                    'Categories',
+                    style: TextStyle(
+                        color: AppColors.brandDarkColor, fontSize: 14),
+                  ),
+                )),
+            ElevatedButton(
+                style: recentTransactionsBtnStyle,
+                onPressed: recentBtnAction,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                  child: Text(
+                    'Recent transaction',
+                    style: TextStyle(
+                        color: AppColors.brandLigthDarkColor, fontSize: 14),
+                  ),
+                )),
+          ],
+        ));
+  }
+}
+
+class CategoriesWidget extends StatelessWidget {
+  const CategoriesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        TextButton(
+          style: const ButtonStyle(alignment: Alignment.centerRight),
+          child: const Text(
+            'View All',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(53, 97, 254, 1)),
+          ),
+          onPressed: () {
+            print('le diste clic a view all');
+          },
+        ),
+        const ProductDetailCard(
+          pathToProductImage: 'assets/images/pizza.png',
+          amount: 391254.01,
+          productName: 'Food & Drink',
+          currentSells: '2250',
+          percentage: '1.8',
+          typeProductDetailCard: TypeProductDetailCard.incomes,
+        ),
+        const ProductDetailCard(
+          typeProductDetailCard: TypeProductDetailCard.incomes,
+          pathToProductImage: 'assets/images/tv.png',
+          amount: 3176254.01,
+          productName: 'Electronics',
+          currentSells: '2250',
+          percentage: '43.6',
+        ),
+        const ProductDetailCard(
+          typeProductDetailCard: TypeProductDetailCard.outcomes,
+          pathToProductImage: 'assets/images/health.png',
+          amount: 38.01,
+          productName: 'Health',
+          currentSells: '110',
+          percentage: '25.8',
+        ),
+      ],
+    );
+  }
+}
+
+class RecentTransactions extends StatelessWidget {
+  const RecentTransactions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '',
+        style: Theme.of(context).textTheme.headlineLarge,
+      ),
     );
   }
 }
